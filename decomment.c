ced9427a5d8be3ae78c5dcf61bb7e9e1c0182d7a
Baris Onat <barisonat@dynamic-oit-vapornet100-10-8-31-48.princeton.edu>
2021-09-21 03:03:51 -0400

    Added comments. Added the readme. Minor update in the dfa.

--- decomment.c
+++ decomment.c
@@ -1,18 +1,21 @@
 /*------------------------------------------------------------*/
 /* decomment.c */ 
-/* Author: Bob Dondero */ 
+/* Author: Baris Onat */ 
 /*------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+// A global variable Statetype for storing the current state to use in
+// the DFA
 enum Statetype {NORMAL, START_SLASH, COMMENT, END_STAR, STR_LITERAL, 
 CHAR_LITERAL, STR_ESCAPE, CHAR_ESCAPE};
 
 /*----------------------------------------------------------*/
 /* Implement the NORMAL state of the DFA. c is the current
-   DFA character. Write c or its uppercase equivalent to
-   stdout, as specified by the DFA. Return the next state. */
+   DFA character. Decide what to write depending on the DFA 
+   instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleNormalState(int c){
     enum Statetype state;
     if (c == '\'') {
@@ -24,26 +27,34 @@ enum Statetype handleNormalState(int c){
         state = STR_LITERAL;
     }
     else if (c == '/'){
+        // c should not be written if a star follows the slash
         state = START_SLASH;
     }
     else{
         putchar(c);
         state = NORMAL;
     }
-
     return state;
 }
 
+/*----------------------------------------------------------*/
+/* Implement the START_SLASH state of the DFA. c is the 
+   current DFA character. Decide what to write depending on 
+   the DFA instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleStartSlashState(int c){
     enum Statetype state;
-    if (c == '\'') {
+    if (c == '*'){
+        putchar(' '); // replace the comment with a space
+        state = COMMENT;
+    }
+
+    // If a star does not follow the slash character, we should write
+    // the slash character.
+    else if (c == '\'') {
         putchar('/');
         putchar(c);
         state = CHAR_LITERAL;
-    }
-    else if (c == '*'){
-        putchar(' ');
-        state = COMMENT;
     }
     else if (c == '"') {
         putchar('/');
@@ -51,7 +62,7 @@ enum Statetype handleStartSlashState(int c){
         state = STR_LITERAL;
     }
     else if (c == '/'){
-        putchar(c);
+        putchar('/');
         state = START_SLASH;
     }
     else{
@@ -59,10 +70,14 @@ enum Statetype handleStartSlashState(int c){
         putchar(c);
         state = NORMAL;
     }
-
     return state;
 }
 
+/*----------------------------------------------------------*/
+/* Implement the COMMENT state of the DFA. c is the current 
+   DFA character. Decide what to write depending on the DFA
+   instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleCommentState(int c){
     enum Statetype state;
     if (c == '*') {
@@ -74,6 +89,11 @@ enum Statetype handleCommentState(int c){
     return state;
 }
 
+/*----------------------------------------------------------*/
+/* Implement the END_STAR state of the DFA. c is the current
+   DFA character. Decide what to write depending on the DFA
+   instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleEndStarState(int c){
     enum Statetype state;
     if (c == '/'){
@@ -88,6 +108,11 @@ enum Statetype handleEndStarState(int c){
     return state;
 }
 
+/*----------------------------------------------------------*/
+/* Implement the CHAR_LITERAL state of the DFA. c is the 
+   current DFA character. Decide what to write depending on 
+   the DFA instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleCharLiteralState(int c){
     enum Statetype state;
     if (c == '\\') {
@@ -102,10 +127,14 @@ enum Statetype handleCharLiteralState(int c){
         putchar(c);
         state = CHAR_LITERAL;
     }
-
     return state;
 }
 
+/*----------------------------------------------------------*/
+/* Implement the STR_LITERAL state of the DFA. c is the 
+   current DFA character. Decide what to write depending on 
+   the DFA instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleStrLiteralState(int c){
     enum Statetype state;
     if (c == '\\') {
@@ -120,80 +149,100 @@ enum Statetype handleStrLiteralState(int c){
         putchar(c);
         state = STR_LITERAL;
     }
-
     return state;
 }
 
+/*----------------------------------------------------------*/
+/* Implement the CHAR_ESCAPE state of the DFA. c is the 
+   current DFA character. Decide what to write depending on 
+   the DFA instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleCharEscapeState(int c){
     putchar(c);
     return CHAR_LITERAL;
 }
 
+/*----------------------------------------------------------*/
+/* Implement the STR_ESCAPE state of the DFA. c is the 
+   current DFA character. Decide what to write depending on 
+   the DFA instructions. Return the next state. */
+/*----------------------------------------------------------*/ 
 enum Statetype handleStrEscapeState(int c){
     putchar(c);
     return STR_LITERAL;
 }
 
 /*----------------------------------------------------------*/
-/* Read text from stdin. Convert the first character of each
-   "word" to uppercase, where a word is a sequence of
-   letters. Write the result to stdout. Return 0. */
+/* Read text from stdin. Decomment the program (remove 
+   comments defined in C90 standards). Return EXIT_SUCCESS
+   if the program doesn't contain an unterminated comment.
+   If it does, return EXIT_FAILURE and write stderr at what
+   line the unterminated comment starts. */
+/*----------------------------------------------------------*/
 int main(void){
-    /* Use a DFA approach.  state indicates the DFA state. */
+    /* State indicates the current DFA state. */
     enum Statetype state = NORMAL;
-    int iChar;
-    int iLineCount;
-    int iLineCountInComment;
+    int c; // the current char
+    int lineCount; // the number of new line characters in the text
+    // the number of new line characters in the most recent comment
+    int lineCountInComment; 
     
-    iLineCount = 1;
-    iLineCountInComment = 0;
+    lineCount = 1;
+    lineCountInComment = 0;
 
-    while ((iChar = getchar()) != EOF) {
-        if(iChar == '\n'){
-            if(state == COMMENT || state == END_STAR){
-                iLineCountInComment++;
-                putchar(iChar);
+    while ((c = getchar()) != EOF) {
+        if (c == '\n'){
+            if (state == COMMENT || state == END_STAR){
+                lineCountInComment++;
+                putchar(c);
             }
             else{
-                iLineCountInComment = 0;
+                /* The lineCountInComment counts the number of new
+                   line characters observed in the most recent comment
+                   in case there is an unterminated comment. But the
+                   fact that the program is in the else statement shows
+                   that the text is out of the comment. */
+                lineCountInComment = 0;
             }
-            iLineCount++;
+            lineCount++;
         }
         switch (state) {
             case NORMAL:
-                state = handleNormalState(iChar);
+                state = handleNormalState(c);
                 break;
             case START_SLASH:
-                state = handleStartSlashState(iChar);
+                state = handleStartSlashState(c);
                 break;
             case COMMENT:
-                state = handleCommentState(iChar);
+                state = handleCommentState(c);
                 break;
             case END_STAR:
-                state = handleEndStarState(iChar);
+                state = handleEndStarState(c);
                 break;
             case CHAR_LITERAL:
-                state = handleCharLiteralState(iChar);
+                state = handleCharLiteralState(c);
                 break;
             case STR_LITERAL:
-                state = handleStrLiteralState(iChar);
+                state = handleStrLiteralState(c);
                 break;
             case CHAR_ESCAPE:
-                state = handleCharEscapeState(iChar);
+                state = handleCharEscapeState(c);
                 break;
             case STR_ESCAPE:
-                state = handleStrEscapeState(iChar);
+                state = handleStrEscapeState(c);
                 break;
         }
     }
 
+    // If the last character is a slash character, the DFA will not
+    // print a slash due to its nature. It should print it here.
     if (state == START_SLASH){
         putchar('/');
     }
     
     if (state == COMMENT || state == END_STAR){
         fprintf( stderr, "Error: line %d: unterminated comment\n", 
-        iLineCount - iLineCountInComment);
+        lineCount - lineCountInComment);
         return EXIT_FAILURE;
     } 
     else{
